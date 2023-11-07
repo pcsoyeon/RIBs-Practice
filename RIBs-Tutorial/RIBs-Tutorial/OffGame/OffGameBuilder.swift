194238a4a76d38bc2a6dd2e1d14f83e0c7e97af0
kim so yeon <sykim@autocrpt.io>
2023-11-07 15:57:53 +0900

    add : RootRIB -> LoggedInRIB 데이터 전달

--- RIBs-Tutorial/RIBs-Tutorial/OffGame/OffGameBuilder.swift
+++ RIBs-Tutorial/RIBs-Tutorial/OffGame/OffGameBuilder.swift
@@ -8,8 +8,8 @@
 import RIBs
 
 protocol OffGameDependency: Dependency {
-    // TODO: Declare the set of dependencies required by this RIB, but cannot be
-    // created by this RIB.
+    var player1Name: String { get }
+    var player2Name: String { get }
 }
 
 final class OffGameComponent: Component<OffGameDependency> {
@@ -24,6 +24,14 @@ protocol OffGameBuildable: Buildable {
 }
 
 final class OffGameBuilder: Builder<OffGameDependency>, OffGameBuildable {
+    
+    fileprivate var player1Name: String {
+        return dependency.player1Name
+    }
+    
+    fileprivate var player2Name: String {
+        return dependency.player2Name
+    }
 
     override init(dependency: OffGameDependency) {
         super.init(dependency: dependency)
@@ -31,7 +39,7 @@ final class OffGameBuilder: Builder<OffGameDependency>, OffGameBuildable {
 
     func build(withListener listener: OffGameListener) -> OffGameRouting {
         let component = OffGameComponent(dependency: dependency)
-        let viewController = OffGameViewController()
+        let viewController = OffGameViewController(player1Name: player1Name, player2Name: player2Name)
         let interactor = OffGameInteractor(presenter: viewController)
         interactor.listener = listener
         return OffGameRouter(interactor: interactor, viewController: viewController)
